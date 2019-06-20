7640f4ae8fedb02626fe12e7f87c2cdf0be96b05
sazhod <46746812+sazhod@users.noreply.github.com>
2019-06-20 19:16:33 +0500

    Add files via upload

---
+++ lab3/public/js/custom.js
@@ -0,0 +1,65 @@
+
+/*let per = document.getElementsByClassName("Logotip");
+for(let i = 0; i< per.length;i++){
+    per[i].style.width = per[i].style.height;
+}*/
+/*const $fun1 = function(){
+    let $per1 = document.getElementsByClassName("Logotip");
+    for(let i = 0; i< $per1.length;i++){
+        $per1[i].style.border = "0";
+    }
+};
+const $fun2= function(){
+    $("#Log1")[0].style.borderLeft = "1px solid rgba(109,110,113,0.5)";
+    $("#Log2")[0].style.borderLeft = "1px solid rgba(109,110,113,0.5)";
+    $("#Log3")[0].style.borderLeft = "1px solid rgba(109,110,113,0.5)";
+    $("#last")[0].style.borderLeft = "1px solid rgba(109,110,113,0.5)";
+    $("#last")[0].style.borderRight = "1px solid rgba(109,110,113,0.5)";
+};
+let $Logotip= $('#Log1');
+$Logotip.on("mouseenter",function () {
+    $fun1();
+    $Logotip[0].style.borderLeft = "1px solid rgba(109,110,113,0.5)";
+    $Logotip[0].style.borderRight = "1px solid rgba(109,110,113,0.5)";
+
+});
+$Logotip.on("mouseleave",function () {
+    $fun1();
+    $fun2();
+});
+
+let $Logotip2= $('#Log2');
+$Logotip2.on("mouseenter",function () {
+    $fun1();
+    $Logotip2[0].style.borderLeft = "1px solid rgba(109,110,113,0.5)";
+    $Logotip2[0].style.borderRight = "1px solid rgba(109,110,113,0.5)";
+
+});
+$Logotip2.on("mouseleave",function () {
+    $fun1();
+    $fun2();
+});
+
+let $Logotip3= $('#Log3');
+$Logotip3.on("mouseenter",function () {
+    $fun1();
+    $Logotip3[0].style.borderLeft = "1px solid rgba(109,110,113,0.5)";
+    $Logotip3[0].style.borderRight = "1px solid rgba(109,110,113,0.5)";
+
+});
+$Logotip3.on("mouseleave",function () {
+    $fun1();
+    $fun2();
+});
+
+let $Logotip4= $('#last');
+$Logotip4.on("mouseenter",function () {
+    $fun1();
+    $Logotip4[0].style.borderLeft = "1px solid rgba(109,110,113,0.5)";
+    $Logotip4[0].style.borderRight = "1px solid rgba(109,110,113,0.5)";
+
+});
+$Logotip4.on("mouseleave",function () {
+    $fun1();
+    $fun2();
+});*/
